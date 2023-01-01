b1c04baa8627b3336e5485550201904a2c64e366
LODYGENSKY Oleg <oleg.lodygensky@lal.in2p3.fr>
2023-01-01 12:18:44 +0100

    still under dev

--- frontend/lifecyclenft/src/app/app.component.ts
+++ frontend/lifecyclenft/src/app/app.component.ts
@@ -1,10 +1,11 @@
 //import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ethers, Wallet } from 'ethers';
+import { BigNumber, ethers, Wallet } from 'ethers';
 import detectEthereumProvider from '@metamask/detect-provider';
 import nftJson from '../assets/contracts/LifeCycleNFT.json'
 import lifecycleMgrJson from '../assets/contracts/LifeCycleManager.json'
 import { environment } from "../environments/environment";
+import { bufferToggle } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -22,89 +23,147 @@ export class AppComponent {
   nftContractAddr: string;
   lifeCycleMgrContract: ethers.Contract | undefined;
   lifeCycleMgrAddr: string;
+  nftIds: Uint8Array[] | undefined;
 
-  //constructor(private http: HttpClient) {
-  constructor() {
-    const provider = detectEthereumProvider();
 
-    this.lifeCycleMgrAddr = environment.LIFECYCLEMGR_ADDR;
-    this.nftContractAddr = environment.NFT_ADDR;
 
-    if (!provider) {
-      throw new Error('Please install MetaMask!');
-    }
+  hiddens = [
+    false, // home
+    true,  //history
+    true  // about
+  ]
 
-    console.log(provider);
-    this.provider = new ethers.providers.Web3Provider(window.ethereum);
-    console.log(this.provider);
-    if (typeof window.ethereum == 'undefined') {
-      console.log('MetaMask is not installed!');
-      throw new Error('MetaMask is installed!');
-    }
-    this.metamask = true;
+currentPage = "home";
 
+getPageIndex(what: string) {
+  switch(what) {
+    case "home": return 0;
+    case "history": return 1;  
+    case "about": return 2;
+    default: return 0;
+  }
+}
+toggle(what: string) {
+  if (what == this.currentPage) 
+    return;
+
+  const element = document.getElementById(what);
+  if (!element)
+    throw new Error("element not found " + what)
+
+  const currentElement = document.getElementById(this.currentPage);
+  if(!currentElement) 
+    return;
+
+  currentElement.style.visibility = 'hidden';
+  this.hiddens[this.getPageIndex(this.currentPage)] = true;
+  this.hiddens[this.getPageIndex(what)] = false;
+
+  if (this.hiddens[this.getPageIndex(what)] == false) {
+    element.style.visibility = 'visible';
   }
 
-  /**
-   * This connects metamask wallet to this dapp
-   */
-  connect() {
-    const signer = this.provider.getSigner();
-    console.log(signer);
-    window.ethereum.request({ method: 'eth_requestAccounts' }).then(() => {
-      signer.getAddress().then((adresse) => {
-        console.log("[connect] " + adresse)
+  this.currentPage = what;
+}
 
-        this.nftContract = new ethers.Contract(this.nftContractAddr,
-          nftJson.abi,
-          signer);
-        this.lifeCycleMgrContract = new ethers.Contract(this.lifeCycleMgrAddr,
-          lifecycleMgrJson.abi,
-          signer);
-        this.connected = true;
-      })
-    })
+ toggleHome() {
+  this.toggle("home");
+}
+ toggleHistory() {
+  this.toggle("history");
+}
+ toggleAbout() {
+  this.toggle("about");
+}
+
+//constructor(private http: HttpClient) {
+constructor() {
+  const provider = detectEthereumProvider();
+
+  this.lifeCycleMgrAddr = environment.LIFECYCLEMGR_ADDR;
+  this.nftContractAddr = environment.NFT_ADDR;
+
+  if (!provider)
+    throw new Error('Please install MetaMask!');
+
+  console.log(provider);
+  this.provider = new ethers.providers.Web3Provider(window.ethereum);
+  console.log(this.provider);
+  if (typeof window.ethereum == 'undefined') {
+    console.log('MetaMask is not installed!');
+    throw new Error('MetaMask is installed!');
   }
-  /**
-   * This retreives NFT list
-   */
-  getNFTList() {
-    const signer = this.provider.getSigner();
+  this.metamask = true;
 
+}
+
+/**
+ * This connects metamask wallet to this dapp
+ */
+connect() {
+  const signer = this.provider.getSigner();
+  console.log(signer);
+  window.ethereum.request({ method: 'eth_requestAccounts' }).then(() => {
     signer.getAddress().then((adresse) => {
-      console.log("[getNFTList] " + adresse);
-      if (!this.lifeCycleMgrContract)
-        throw new Error("LifeCycle Mgr Contract not found");
+      console.log("[connect] " + adresse)
 
-      const nftids = this.lifeCycleMgrContract.connect(signer)['getIds']().then((ids: BigInteger[]) => {
-        if (ids) {
-          for (let i = 0; i < ids.length; i++) {
-            console.log("[getNFTList] " + ids[i]);
-          }
-        }
-        else {
-          console.log("[getNFTList() ] aucun NFT ");
-        }
-      })
+      this.nftContract = new ethers.Contract(this.nftContractAddr,
+        nftJson.abi,
+        signer);
+      this.lifeCycleMgrContract = new ethers.Contract(this.lifeCycleMgrAddr,
+        lifecycleMgrJson.abi,
+        signer);
+      this.connected = true;
     })
-  }
+  })
+}
+/**
+ * This retreives NFT list
+ */
+getNFTList() {
+  const signer = this.provider.getSigner();
 
-  /**
-   * This mints a new  NFT
-   */
-  mintNFT() {
-    const signer = this.provider.getSigner();
+  signer.getAddress().then((adresse) => {
+    console.log("[getNFTList] " + adresse);
+    if (!this.lifeCycleMgrContract)
+      throw new Error("LifeCycle Mgr Contract not found");
 
-    signer.getAddress().then((adresse) => {
-      if (!this.lifeCycleMgrContract)
-        throw new Error("LifeCycle Mgr Contract not found");
-
-      console.log("[mintNFT] " + adresse);
-
-      this.lifeCycleMgrContract.connect(signer)['mintNFT']().then(() => {
-        console.log("[mintNFT] done");
-      })
+    const nftids = this.lifeCycleMgrContract.connect(signer)['getIds']().then((ids: BigInteger[]) => {
+      if (ids) {
+        this.nftIds = ids;
+        for (let i = 0; i < ids.length; i++) {
+          console.log("[getNFTList] nftIds[" + i + "] = " + this.nftIds[i]);
+        }
+      }
+      else {
+        console.log("[getNFTList() ] aucun NFT ");
+      }
     })
-  }
+  })
+}
+/**
+  * This retreives NFT history
+  */
+getNFTHistory() {
+  this.toggleHistory();
+}
+
+/**
+ * This mints a new  NFT
+ */
+mintNFT() {
+  const signer = this.provider.getSigner();
+
+  signer.getAddress().then((adresse) => {
+    if (!this.lifeCycleMgrContract)
+      throw new Error("LifeCycle Mgr Contract not found");
+
+    console.log("[mintNFT] " + adresse);
+
+    this.lifeCycleMgrContract.connect(signer)['mintNFT']().then(() => {
+      console.log("[mintNFT] done");
+    })
+  })
+}
 }
 
