7f72b8134a1814ee0e992e915a57c25982191f27
LODYGENSKY Oleg <oleg.lodygensky@lal.in2p3.fr>
2023-01-02 23:04:15 +0100

    do action

--- frontend/lifecyclenft/src/app/app.component.ts
+++ frontend/lifecyclenft/src/app/app.component.ts
@@ -24,146 +24,219 @@ export class AppComponent {
   lifeCycleMgrContract: ethers.Contract | undefined;
   lifeCycleMgrAddr: string;
   nftIds: Uint8Array[] | undefined;
-
+  currentToken: Uint8Array | undefined;
 
 
   hiddens = [
-    false, // home
-    true,  //history
-    true  // about
+    false, // 0 : home
+    true,  // 1 : history
+    true,  // 2 : doSomething
+    true   // 3 : about
   ]
 
-currentPage = "home";
+  currentPage = "home";
 
-getPageIndex(what: string) {
-  switch(what) {
-    case "home": return 0;
-    case "history": return 1;  
-    case "about": return 2;
-    default: return 0;
-  }
-}
-toggle(what: string) {
-  if (what == this.currentPage) 
-    return;
-
-  const element = document.getElementById(what);
-  if (!element)
-    throw new Error("element not found " + what)
-
-  const currentElement = document.getElementById(this.currentPage);
-  if(!currentElement) 
-    return;
-
-  currentElement.style.visibility = 'hidden';
-  this.hiddens[this.getPageIndex(this.currentPage)] = true;
-  this.hiddens[this.getPageIndex(what)] = false;
-
-  if (this.hiddens[this.getPageIndex(what)] == false) {
-    element.style.visibility = 'visible';
+  getPageIndex(what: string) {
+    switch (what) {
+      case "home": return 0;
+      case "history": return 1;
+      case "dosomething": return 2;
+      case "about": return 3;
+      default: return 0;
+    }
   }
 
-  this.currentPage = what;
-}
+  /**
+   * This shows/hides an element
+   * @param what is the name of the element to show
+   * @returns 
+   */
+  toggle(what: string) {
+    if (what == this.currentPage)
+      return;
 
- toggleHome() {
-  this.toggle("home");
-}
- toggleHistory() {
-  this.toggle("history");
-}
- toggleAbout() {
-  this.toggle("about");
-}
+    const element = document.getElementById(what);
+    if (!element)
+      throw new Error("element not found " + what)
 
-//constructor(private http: HttpClient) {
-constructor() {
-  const provider = detectEthereumProvider();
+    const currentElement = document.getElementById(this.currentPage);
+    if (!currentElement)
+      return;
 
-  this.lifeCycleMgrAddr = environment.LIFECYCLEMGR_ADDR;
-  this.nftContractAddr = environment.NFT_ADDR;
+    currentElement.style.visibility = 'hidden';
+    this.hiddens[this.getPageIndex(this.currentPage)] = true;
+    this.hiddens[this.getPageIndex(what)] = false;
 
-  if (!provider)
-    throw new Error('Please install MetaMask!');
+    if (this.hiddens[this.getPageIndex(what)] == false) {
+      element.style.visibility = 'visible';
+    }
 
-  console.log(provider);
-  this.provider = new ethers.providers.Web3Provider(window.ethereum);
-  console.log(this.provider);
-  if (typeof window.ethereum == 'undefined') {
-    console.log('MetaMask is not installed!');
-    throw new Error('MetaMask is installed!');
+    this.currentPage = what;
   }
-  this.metamask = true;
+  /** 
+   * This calls toggle("home") 
+   */
+  toggleHome() {
+    this.toggle("home");
+  }
+  /** 
+   * This calls toggle("history") 
+   */
+  toggleHistory(id: Uint8Array) {
+    const what = "history";
+    this.currentToken = id;
+    this.toggle(what);
+  }
+  /** 
+   * This prepares "dosomething" element and calls toggle("dosomething") 
+   * @param id is saved in currentToken
+   */
+  toggleDoSomething(id: Uint8Array) {
+    const what = "dosomething";
+    this.currentToken = id;
+    this.toggle(what);
+  }
+  /** 
+   * This calls toggle("about") 
+   */
 
-}
+  toggleAbout() {
+    this.toggle("about");
+  }
 
-/**
- * This connects metamask wallet to this dapp
- */
-connect() {
-  const signer = this.provider.getSigner();
-  console.log(signer);
-  window.ethereum.request({ method: 'eth_requestAccounts' }).then(() => {
+  //constructor(private http: HttpClient) {
+  constructor() {
+    const provider = detectEthereumProvider();
+
+    this.lifeCycleMgrAddr = environment.LIFECYCLEMGR_ADDR;
+    this.nftContractAddr = environment.NFT_ADDR;
+
+    if (!provider)
+      throw new Error('Please install MetaMask!');
+
+    console.log(provider);
+    this.provider = new ethers.providers.Web3Provider(window.ethereum);
+    console.log(this.provider);
+    if (typeof window.ethereum == 'undefined') {
+      console.log('MetaMask is not installed!');
+      throw new Error('MetaMask is installed!');
+    }
+    this.metamask = true;
+
+  }
+
+  /**
+   * This connects metamask wallet to this dapp
+   */
+  connect() {
+    const signer = this.provider.getSigner();
+    console.log(signer);
+    window.ethereum.request({ method: 'eth_requestAccounts' }).then(() => {
+      signer.getAddress().then((adresse) => {
+        console.log("[connect] " + adresse)
+
+        this.nftContract = new ethers.Contract(this.nftContractAddr,
+          nftJson.abi,
+          signer);
+        this.lifeCycleMgrContract = new ethers.Contract(this.lifeCycleMgrAddr,
+          lifecycleMgrJson.abi,
+          signer);
+        this.connected = true;
+      })
+    })
+  }
+  /**
+   * This retreives NFT list
+   */
+  getNFTList() {
+    const signer = this.provider.getSigner();
+
     signer.getAddress().then((adresse) => {
-      console.log("[connect] " + adresse)
+      console.log("[getNFTList] " + adresse);
+      if (!this.lifeCycleMgrContract)
+        throw new Error("LifeCycle Mgr Contract not found");
 
-      this.nftContract = new ethers.Contract(this.nftContractAddr,
-        nftJson.abi,
-        signer);
-      this.lifeCycleMgrContract = new ethers.Contract(this.lifeCycleMgrAddr,
-        lifecycleMgrJson.abi,
-        signer);
-      this.connected = true;
-    })
-  })
-}
-/**
- * This retreives NFT list
- */
-getNFTList() {
-  const signer = this.provider.getSigner();
-
-  signer.getAddress().then((adresse) => {
-    console.log("[getNFTList] " + adresse);
-    if (!this.lifeCycleMgrContract)
-      throw new Error("LifeCycle Mgr Contract not found");
-
-    const nftids = this.lifeCycleMgrContract.connect(signer)['getIds']().then((ids: BigInteger[]) => {
-      if (ids) {
-        this.nftIds = ids;
-        for (let i = 0; i < ids.length; i++) {
-          console.log("[getNFTList] nftIds[" + i + "] = " + this.nftIds[i]);
+      const nftids = this.lifeCycleMgrContract.connect(signer)['getIds']().then((ids: BigInteger[]) => {
+        if (ids) {
+          this.nftIds = ids;
+          for (let i = 0; i < ids.length; i++) {
+            console.log("[getNFTList] nftIds[" + i + "] = " + this.nftIds[i]);
+          }
         }
-      }
-      else {
-        console.log("[getNFTList() ] aucun NFT ");
-      }
+        else {
+          console.log("[getNFTList() ] aucun NFT ");
+        }
+      })
     })
-  })
-}
-/**
-  * This retreives NFT history
-  */
-getNFTHistory() {
-  this.toggleHistory();
-}
+  }
+  /**
+    * This retreives NFT history from the backend
+    */
+  getNFTHistory(id: Uint8Array) {
+    this.toggleHistory(id);
+  }
 
-/**
- * This mints a new  NFT
- */
-mintNFT() {
-  const signer = this.provider.getSigner();
+  /**
+    * This registers an action on the blockchain; potentialy saves it to the backend
+    * @param id : the tokenid
+    * @param message: the action message
+    * @param save: if true, the message is sent to the backend
+    */
+  //doSomething(id : Uint8Array, message : string, save : boolean) {
+  doSomething() {
+    const signer = this.provider.getSigner();
 
-  signer.getAddress().then((adresse) => {
-    if (!this.lifeCycleMgrContract)
-      throw new Error("LifeCycle Mgr Contract not found");
+    if (!this.currentToken) {
+      console.log("[doSomething] currentToken not set");
+      return;
+    }
 
-    console.log("[mintNFT] " + adresse);
+    console.log("[doSomething] tokenId " + this.currentToken);
 
-    this.lifeCycleMgrContract.connect(signer)['mintNFT']().then(() => {
-      console.log("[mintNFT] done");
+    let what = "doSomethingMessage";
+    const elementMsg = document.getElementById(what) as HTMLInputElement;
+    if (!elementMsg)
+      throw new Error("element not found " + what)
+
+    console.log("[doSomething] message " + elementMsg.value);
+
+    what = "doSomethingSave";
+    const elementSave = document.getElementById(what) as HTMLInputElement;
+    if (!elementMsg)
+      throw new Error("element not found " + what)
+
+    console.log("[doSomething] save " + elementSave.checked);
+
+/*
+      signer.getAddress().then((adresse) => {
+      if (!this.lifeCycleMgrContract)
+        throw new Error("LifeCycle Mgr Contract not found");
+
+      console.log("[mintNFT] " + adresse);
+
+      this.lifeCycleMgrContract.connect(signer)['doSomething'](id, message).then(() => {
+        console.log("[mintNFT] done");
+      })
     })
-  })
-}
+*/
+  }
+
+  /**
+   * This mints a new  NFT
+   */
+  mintNFT() {
+    const signer = this.provider.getSigner();
+
+    signer.getAddress().then((adresse) => {
+      if (!this.lifeCycleMgrContract)
+        throw new Error("LifeCycle Mgr Contract not found");
+
+      console.log("[mintNFT] " + adresse);
+
+      this.lifeCycleMgrContract.connect(signer)['mintNFT']().then(() => {
+        console.log("[mintNFT] done");
+      })
+    })
+  }
 }
 
